41b1c963f6a003ce0eaeca0814f77f5696b7aaf9
Bruno Amorim <brunoamorim14@gmail.com>
2024-05-10 02:27:48 -0300

    feat: Add TransferDto for transferring funds

---
+++ Presentation/Controllers/Dtos/Transactions/TransferDto.cs
@@ -0,0 +1,14 @@
+﻿namespace api.Presentation;
+
+public class AccountDto
+{
+    public string? AccountDigit { get; set; }
+    public string? AccountNumber { get; set; }
+}
+public class TransferDto
+{
+    public decimal Amount { get; set; }
+    public string? EmailDestination { get; set; }
+    public AccountDto? AccountDestination { get; set; }
+
+}
