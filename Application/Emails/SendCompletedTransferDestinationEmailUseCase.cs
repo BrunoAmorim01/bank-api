749ffdba032f4bc01af024f5a7480ae262a8ed38
Bruno Amorim <brunoamorim14@gmail.com>
2024-05-11 03:36:07 -0300

    feat: add SendCompletedTransferDestinationEmailUseCase to handle sending email for completed transfer destination

---
+++ Application/Emails/SendCompletedTransferDestinationEmailUseCase.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using api.Domain.Services;
+
+namespace api.Application.Emails;
+
+public class SendCompletedTransferDestinationEmailUseCase(IEMailService emailService, ILogger<SendCompletedTransferDestinationEmailUseCase> logger)
+{
+    public async Task Execute(string email, string nameDestination, int value)
+    {
+        var valueInDecimal = value / 100;
+        var valueInBRL = valueInDecimal.ToString("C", CultureInfo.GetCultureInfo("pt-BR"));
+
+        var mailRequest = new MailRequest
+        {
+            ToEmail = email,
+            Subject = "Transfer received",
+            Body = $"Hello {nameDestination}! You received a transfer of {valueInBRL} successfully."
+        };
+        try
+        {
+            await emailService.SendBasicEmail(mailRequest);
+        }
+        catch (Exception e)
+        {
+            logger.LogError("Error sending email");
+            logger.LogError(e.Message);
+
+        }
+
+    }
+}
