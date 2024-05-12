cebc5a0bd98bc326bda90131356c096ccbacbd23
Bruno Amorim <brunoamorim14@gmail.com>
2024-05-12 03:43:40 -0300

    feat: Add ListTransactionsUseCase to retrieve transactions by user ID

---
+++ Application/Transactions/UseCases/Transactions/ListTransactionsUseCase.cs
@@ -0,0 +1,44 @@
+﻿using api.Domain.Repositories;
+
+namespace api.Application.Transactions;
+
+public class ListTransactionsUseCase(
+    ITransactionRepository transactionRepository
+    )
+{
+    public async Task<Object[]> Execute(Guid userId)
+    {
+        var transactions = await transactionRepository.ListByUserId(userId);
+
+        var response = transactions.Select(x => new
+        {
+            x.Id,
+            x.CreatedAt,
+            amount = x.Value / 100,
+            x.TransactionType,
+            x.TransactionStatus,
+            BankDestination = new
+            {
+
+                x.BankDestination.AccountDigit,
+                x.BankDestination.AccountNumber
+            },
+            BankOrigin = new
+            {
+                x.BankOrigin.AccountDigit,
+                x.BankOrigin.AccountNumber
+            },
+            UserDestination = new
+            {
+                x.UserDestination.Name,
+            },
+            UserOrigin = new
+            {                
+                x.UserOrigin.Name,                
+            }
+        }).ToArray();
+
+        return response;
+    }
+
+}
