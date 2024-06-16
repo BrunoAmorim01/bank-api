a1081c0298dabd260fefaf5963665ca7a1231c92
Bruno Amorim <brunoamorim14@gmail.com>
2024-06-15 22:43:11 -0300

    feat: Add PdfService for generating PDFs from transaction data

---
+++ Domain/Services/Pdf/PdfService.cs
@@ -0,0 +1,18 @@
+﻿
+using api.Domain.Enums;
+
+namespace api.Domain.Services.Pdf;
+public class ExportListTransactions
+{
+    public required DateTime CreatedAt { get; set; }
+    public TransactionTypeEnum TransactionType { get; set; }
+    public TransactionStatusEnum TransactionStatus { get; set; }
+    public int Value { get; set; }
+    public required string FromUser { get; set; }
+    public required string ToUser { get; set; }
+}
+public interface IPdfService
+{
+     byte[] GeneratePdf(ExportListTransactions[] data);
+
+}
