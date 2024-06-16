d52ba4246d7ee1c195b391c1e0efb5c289728fe9
Bruno Amorim <brunoamorim14@gmail.com>
2024-06-15 22:43:22 -0300

    feat: Add ExportListTransactionsUseCase for exporting transaction data as PDF

---
+++ Application/Transactions/UseCases/Transactions/ExportListTransactionsUseCase.cs
@@ -0,0 +1,41 @@
+﻿using api.Application.Transactions.Interfaces;
+using api.Domain.Repositories;
+using api.Domain.Services.Pdf;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Application.Transactions;
+
+
+public class ExportListTransactionsUseCase(
+    ITransactionRepository transactionRepository,
+    IPdfService pdfService
+    )
+{
+    public async Task<byte[]> Execute(Guid userId, TransactionsListRequest query)
+    {
+        var transactions = await transactionRepository.ListByUserId(userId, new FindQuery
+        {
+            StartDate = query.StartDate,
+            EndDate = query.EndDate,
+            TransactionType = query.TransactionType,
+            TransactionStatus = query.TransactionStatus
+        });
+
+        var response = transactions.Select(x => new ExportListTransactions
+        {
+            CreatedAt = TimeZoneInfo.ConvertTimeFromUtc(x.CreatedAt, TimeZoneInfo.FindSystemTimeZoneById("Central Brazilian Standard Time")),
+            TransactionType = x.TransactionType,
+            TransactionStatus = x.TransactionStatus,
+            Value = x.Value,
+            FromUser = x.UserOrigin.Name,
+            ToUser = x.UserDestination.Name
+        }).ToArray();
+
+
+        var pdfContent = pdfService.GeneratePdf(response);
+
+        return pdfContent;
+    }
+
+}
+
